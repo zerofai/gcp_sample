e6a08501a86ef5c70fe9238571d8723a80080ea4
zerofai <zerofai@gmail.com>
2022-03-23 13:18:22 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,29 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "gcp_network" "default" {
+  auto_create_subnetworks         = "false"
+  delete_default_routes_on_create = "false"
+  description                     = "Default network for the project"
+  mtu                             = "0"
+  name                            = "default"
+  project                         = "example-project"
+  routing_mode                    = "REGIONAL"
+}
+
+resource "gcp_firewall" "default-allow-ssh" {
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+
+  direction     = "INGRESS"
+  disabled      = "false"
+  name          = "default-allow-ssh"
+  network       = "${data.terraform_remote_state.local.outputs.google_compute_network_default_self_link}"
+  priority      = "1000"
+  project       = "example-project"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-server"]
+}
